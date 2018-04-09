6dd44a3beb4175be735783541d581f2c7033cb19
Ian_Buitrago <kieldro@gmail.com>
2018-04-09 15:21:55 -0400

    user class

--- BankApp.java
+++ BankApp.java
@@ -11,6 +11,26 @@ public class BankApp {
 		
 		String name = sc.nextLine();
 		System.out.println("Entered name: " + name);
+		User u = new User(name);
+		System.out.println(name + " your options are:");
+		System.out.println(name + " your balance is : $" + u.balance);
+		System.out.println("-Enter 1 for deposits");
+		System.out.println("-Enter 2 for withdrawals");
+		
+		int  prompt = sc.nextInt();
+		if (prompt == 1) {
+			System.out.println("Enter the amount to deposit: ");
+			System.out.print("$");
+			float deposit = sc.nextFloat();
+			u.deposit(deposit);
+		}else if (prompt == 2) {
+			System.out.println("Enter the amount to withdraw: ");
+			System.out.print("$");
+			float withdrawal = sc.nextFloat();
+			u.withdraw(withdrawal);
+		}
+		System.out.println(name + " your balance is : $" + u.balance);
+		
 		
 	}
 }
---
+++ User.java
@@ -0,0 +1,18 @@
+package com.revature.project0;
+
+public class User {
+	String name;
+	
+	float balance;
+	public User(String name) {
+		this.name = name;
+	}
+	
+	public void deposit(float deposit) {
+		balance += deposit;
+	}
+	
+	public void withdraw(float withdrawal) {
+		balance -= withdrawal;
+	}
+}
