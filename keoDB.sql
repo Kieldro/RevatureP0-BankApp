def5cd30f854b20fb20544a6d6a1906433b86029
Ian <kieldro@gmail.com>
2018-04-15 12:33:12 -0400

    creates user_account table

--- keoDB.sql
+++ keoDB.sql
@@ -1,1 +1,14 @@
-select * from user;
+SELECT
+    *
+FROM
+    user_account;
+
+CREATE TABLE user_account (
+    name       VARCHAR2(40),
+    balance    FLOAT,
+    admin      INT,
+    approved   INT,
+    CONSTRAINT pk_name PRIMARY KEY ( name )
+);
+
+DROP TABLE user_account;
